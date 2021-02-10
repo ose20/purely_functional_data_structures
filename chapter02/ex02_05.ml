b1e49b390ce3c7ef3ec211a0c27c15c4bbf1c690
ose20 <ayato96bell@gmail.com>
2021-02-10 23:09:38 +0900

    solve ex02_05 b

--- chapter02/ex02_05.ml
+++ chapter02/ex02_05.ml
@@ -32,3 +32,37 @@ let time f =
 (* complete の方が実行時間は若干短いだけど
 ** 末尾再帰じゃないので d = 10^6 で Stack overflow が起きる
 *)
+
+(* b *)
+(* a を拡張して、任意のサイズの平衡木を作るようにする
+** 与えられたノードに対し、2 つの部分木のサイズの差は高々 1 とする 
+** O(log n) でできるようにしたい
+*)
+let create_tree elt size =
+  (* サイズ m とサイズ m+1 の木のペアを作る *)
+  (* 右の木のサイズを優先して大きくする *)
+  let rec create2 = function
+    | 0 -> (Lf, Br(elt, Lf, Lf))
+    | m ->
+        begin
+          match m mod 2 with
+          | 0 -> let (x, y) = create2 (m / 2 - 1)
+              in (Br(elt, y, x), Br(elt, y, y))
+          | _ -> let (x, y) = create2 ((m - 1) / 2)
+              in (Br(elt, y, x), Br(elt, y, y))
+        end
+  in
+  match size with
+  | 0 -> Lf
+  | _ ->
+      begin
+        match size mod 2 with
+        | 0 ->
+            let k = size / 2 in
+            let (x, y) = create2 (k - 1) in
+            Br(elt, y, x)
+        | _ ->
+            let k = (size - 1) / 2 in
+            let (x, _) = create2 k in
+            Br(elt, x, x)
+      end
